3d6a12dbab02bb8dfd6d9d328348ad92834a9486
TheeDouglasAM3 <douglas_champion@hotmail.com>
2019-02-17 20:53:11 -0300

    Update Balance (remove timestamp)

--- laravel-saldo/database/migrations/2019_02_17_204410_create_balances_table.php
+++ laravel-saldo/database/migrations/2019_02_17_204410_create_balances_table.php
@@ -15,7 +15,6 @@ class CreateBalancesTable extends Migration
     {
         Schema::create('balances', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
         });
     }
 
