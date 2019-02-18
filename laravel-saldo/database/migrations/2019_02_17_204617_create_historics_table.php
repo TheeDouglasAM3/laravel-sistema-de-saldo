0fa94f442d5c188a5803ee9e68662d22d6581c58
TheeDouglasAM3 <douglas_champion@hotmail.com>
2019-02-17 21:40:17 -0300

    Update Historics migrate (add new fields)

--- laravel-saldo/database/migrations/2019_02_17_204617_create_historics_table.php
+++ laravel-saldo/database/migrations/2019_02_17_204617_create_historics_table.php
@@ -15,6 +15,14 @@ class CreateHistoricsTable extends Migration
     {
         Schema::create('historics', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('type', ['I', 'O', 'T']);
+            $table->double('amount', 10, 2);
+            $table->double('total_before', 10, 2);
+            $table->double('total_after', 10, 2);
+            $table->integer('user_id_transaction')->nullable();
+            $table->date('date');
             $table->timestamps();
         });
     }
