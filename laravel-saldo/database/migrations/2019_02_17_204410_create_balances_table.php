f35bc40697a42796c7960a48281aadf6d92c44c3
TheeDouglasAM3 <douglas_champion@hotmail.com>
2019-02-17 21:39:48 -0300

    Update Balace migrate (add new fields)

--- laravel-saldo/database/migrations/2019_02_17_204410_create_balances_table.php
+++ laravel-saldo/database/migrations/2019_02_17_204410_create_balances_table.php
@@ -15,6 +15,9 @@ class CreateBalancesTable extends Migration
     {
         Schema::create('balances', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->double('amount', 10, 2);
         });
     }
 
