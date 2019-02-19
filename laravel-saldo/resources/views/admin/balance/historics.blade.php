45c2397194c512dca02ccc8f4967d338b62777a7
TheeDouglasAM3 <douglas_champion@hotmail.com>
2019-02-18 21:12:42 -0300

    Add historics

---
+++ laravel-saldo/resources/views/admin/balance/historics.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title', 'Histórico de Movimentações')
+
+@section('content_header')
+    <h1>Histórico de movimentações</h1>
+    <ol class="breadcrumb">
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Histórico</a></li>
+    </ol>
+@stop
+
+@section('content')
+    <div class="box">
+        <div class="box-header">
+            
+        </div>
+        <div class="box-body">
+            <table class="table table-hover table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Valor</th>
+                        <th>Tipo</th>
+                        <th>Data</th>
+                        <th>Sender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($historics as $his)
+                        <tr>
+                            <td>{{ $his->id }}</td>
+                            <td>{{ number_format($his->amount, 2, ',', '.') }}</td>
+                            <td>{{ $his->type }}</td>
+                            <td>{{ $his->date }}</td>
+                            <td>{{ $his->user_id_transaction }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            
+        </div>
+    </div>
+@stop
